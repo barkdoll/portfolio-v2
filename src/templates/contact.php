8b5b049d34ac12041912cfec5ad1ac5680de2c93
Jesse Barkdoll <jessebarkdoll@gmail.com>
2017-12-03 19:42:55 -0500

    commit changes from local env

--- src/templates/contact.php
+++ src/templates/contact.php
@@ -7,7 +7,7 @@
     <h3>Message</h3><textarea rows="8" cols="40" name="message" required></textarea>
 
     <!-- reCAPTCHA -->
-    <div class="g-recaptcha" data-sitekey="6LfULTsUAAAAAJYyY1gS1i3kLjFjvFpikFmgXWZa"></div>
+    <div class="g-recaptcha" data-sitekey="6LfxejsUAAAAAI576Zvy3Xq8Pk0n6k92O7Dt5D05"></div>
     <div id="feedback"></div>
 
     <input type="submit" value="Send" style="margin: .5rem .5rem 0 0">
